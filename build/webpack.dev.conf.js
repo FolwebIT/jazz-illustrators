76a2b66ea8f5d84f8a89187fb0c29a93067cddb3
folamy <folysco@gmail.com>
2018-04-17 21:20:04 +0100

    Host CHanged

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,7 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
-const HOST = '0.0.0.0'
+const HOST = '127.0.0.1'
 // const PORT = process.env.PORT && Number(process.env.PORT)
 const PORT = 8080
 
